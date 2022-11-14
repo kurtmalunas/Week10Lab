3d7a89dae6d08af0ebd01de09b64b33074852c11
SupremeUserKurt <102764110+SupremeUserKurt@users.noreply.github.com>
2022-11-13 18:59:26 -0700

    Finished Lab 10

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,5 +1,6 @@
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -10,16 +11,33 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Role;
+import models.User;
+import services.AccountService;
 
 public class AdminFilter implements Filter {
-    
-    private static final boolean debug = true;
  
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
-
+            // code that is executed before the servlet
+            HttpServletRequest httpRequest = (HttpServletRequest)request;
+            HttpSession session = httpRequest.getSession();
+       
+            UserDB us = new UserDB();
+            String email = (String)session.getAttribute("email");
+            User user = us.get(email);
+            Role role = user.getRole();
+            
+            if (role.getRoleId() != 1) {
+                HttpServletResponse httpResponse = (HttpServletResponse)response;
+                httpResponse.sendRedirect("notes");
+                return;
+            }
             chain.doFilter(request, response);
     }
 
